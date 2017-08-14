e4cb2f7d5327c0650cc013804506b5d051f0e72e
kotlyamich <kotlyamich@ecs.vuw.ac.nz>
2017-08-14 13:22:40 +1200

    fixed scrolly issue in new observation view

--- KeaWatch/KeaWatch/NewObservationViewController.swift
+++ KeaWatch/KeaWatch/NewObservationViewController.swift
@@ -9,6 +9,13 @@
 import UIKit
 
 class NewObservationViewController: UIViewController {
+    
+    
+    @IBAction func postObservation(_ sender: Any) {
+        // TODO: send image and extra data to server!!!
+        
+        // Then go back to home page with posted notification
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
