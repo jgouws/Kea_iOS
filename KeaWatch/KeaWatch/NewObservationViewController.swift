2cb296ffcd2b9ef15bd684beb49fa68838347712
Nigel Munro <munro98@users.noreply.github.com>
2017-09-15 12:01:11 +1200

    Added new observation view controller

---
+++ KeaWatch/KeaWatch/NewObservationViewController.swift
@@ -0,0 +1,41 @@
+//
+//  NewObservationViewController.swift
+//  KakaGo
+//
+//  Created by Nigel Munro on 15/09/17.
+//  Copyright © 2017 Kea Takers. All rights reserved.
+//
+
+import UIKit
+
+class NewObservationViewController: UIViewController {
+    
+    
+    @IBOutlet weak var observationImageView: UIImageView!
+    @IBOutlet weak var observationTextField: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
