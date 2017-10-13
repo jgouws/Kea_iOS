3f0d56bcf569d7c0b8249e856a28dfc4acd994e3
Michael Kane <kanemich2@ecs.vuw.ac.nz>
2017-10-13 18:07:20 +1300

    Tidy up and rename unit tests

--- KeaWatch/KeaWatchTests/KeaWatchTests.swift
+++ KeaWatch/KeaWatchTests/KeaWatchTests.swift
@@ -52,11 +52,32 @@ class KeaWatchTests: XCTestCase {
         XCTAssertEqual(statusCode, 200)
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testPostRequestConnection() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        // given
+        var request = URLRequest(url: URL(string: "http://localhost:8081/appupload")!)
+        request.httpMethod = "POST"
+        // 1
+        let promise = expectation(description: "Completion handler invoked")
+        var statusCode: Int?
+        var responseError: Error?
+        
+        // when
+        let dataTask = sessionUnderTest.dataTask(with: request) { data, response, error in
+            statusCode = (response as? HTTPURLResponse)?.statusCode
+            responseError = error
+            // 2
+            promise.fulfill()
         }
+        dataTask.resume()
+        // 3
+        waitForExpectations(timeout: 5, handler: nil)
+        
+        // then
+        XCTAssertNil(responseError)
+        XCTAssertEqual(statusCode, 200)
     }
     
 }
